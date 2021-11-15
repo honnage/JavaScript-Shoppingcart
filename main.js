15c8af5f145aa56582e5d22628c2875864a02e6c
honnage <hon_da-1998@hotmail.com>
2021-11-15 22:46:43 +0700

    Function setItems

--- main.js
+++ main.js
@@ -14,13 +14,13 @@ let products = [
     },
     {
         name: 'Grey Tshirt 3',
-        tag: 'greytshirt',
+        tag: 'greytshirt 1',
         price: 15,
         inCart: 0
     },
     {
         name: 'Grey Tshirt 4',
-        tag: 'greytshirt',
+        tag: 'greytshirt 1',
         price: 15,
         inCart: 0
     }
@@ -28,7 +28,7 @@ let products = [
 
 for (let i=0; i < carts.length; i++) {
     carts[i].addEventListener('click', () => {
-        cartNumbers();
+        cartNumbers(products[i]);
     })
 }
 
@@ -40,7 +40,7 @@ function onLoadCartNumbers(){
     }
 }
 
-function cartNumbers(){
+function cartNumbers(product){
     let productNumbers = localStorage.getItem('cartNumder');
 
     productNumbers = parseInt(productNumbers);
@@ -53,6 +53,32 @@ function cartNumbers(){
         localStorage.setItem('cartNumder', 1);
         document.querySelector(".cart span").textContent = 1;
     }
+
+    setItems(product);
+}
+
+function setItems(product){
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+
+    if(cartItems != null){
+
+        if(cartItems[product.tag] == undefined){
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+
+        cartItems[product.tag].inCart += 1;
+    }else{
+        product.inCart = 1;
+        cartItems = {
+            [product.tag]: product
+        }
+    }
+
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
 }
 
 onLoadCartNumbers();
